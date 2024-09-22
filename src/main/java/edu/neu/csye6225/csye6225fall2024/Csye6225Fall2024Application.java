e32c800ff6090e415bc430e9f1c56cb5ddeb0235
dongyul1u <liu.dongyu@northeastern.edu>
2024-09-21 21:37:07 -0400

    init project

---
+++ src/main/java/edu/neu/csye6225/csye6225fall2024/Csye6225Fall2024Application.java
@@ -0,0 +1,13 @@
+package edu.neu.csye6225.csye6225fall2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csye6225Fall2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csye6225Fall2024Application.class, args);
+    }
+
+}
