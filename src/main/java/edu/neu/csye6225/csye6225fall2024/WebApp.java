a7488f69ce3cf47766ef8706d2b4b3fc820a703e
dongyul1u <liu.dongyu@northeastern.edu>
2024-10-10 10:41:03 -0400

    CI test2

--- src/main/java/edu/neu/csye6225/csye6225fall2024/WebApp.java
+++ src/main/java/edu/neu/csye6225/csye6225fall2024/WebApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApp.class, args)
+        SpringApplication.run(WebApp.class, args);
     }
 
 }
