5287c398de4286f1732b50b5123bab466884303d
dongyul1u <liu.dongyu@northeastern.edu>
2024-10-17 04:04:35 -0400

    scritp update

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -3,17 +3,24 @@ set -e
 
 sudo apt-get update
 
+# local nonlogin user and group
+sudo groupadd csye6225
+sudo useradd --system --no-create-home --shell /usr/sbin/nologin -g csye6225 csye6225
+
 # OpenJDK 17
 sudo apt-get install -y openjdk-17-jdk
 
-# Maven
-sudo apt-get install -y maven
+## Maven
+#sudo apt-get install -y maven
 
 # check version
 java -version
-mvn -version
+#mvn -version
 
-echo "Java and Maven installed and configured successfully."
+echo "Java installed and configured successfully."
+
+# unzip tool
+sudo apt-get install -y unzip
 
 ls -al
 
@@ -25,5 +32,8 @@ pwd
 ls
 unzip webapp.zip  -d /home/ubuntu/webapp/
 pwd
+
+# set the owner
+chown -R csye6225:csye6225 /home/ubuntu/webapp
 
 echo "Java JAR file prepared"
